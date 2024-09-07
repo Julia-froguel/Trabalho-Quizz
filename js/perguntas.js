a982b80556f9c36a66a1eb818288fee2683037c9
Julia-froguel <126570953+Julia-froguel@users.noreply.github.com>
2024-09-06 23:00:46 -0300

    Add files via upload

---
+++ js/perguntas.js
@@ -0,0 +1,173 @@
+export const perguntas = [
+    {
+        enunciado: "Qual das seguintes práticas contribui para uma boa alimentação?",
+        alternativas: [
+            {
+                texto: "Incluir uma variedade de frutas e vegetais na dieta.",
+                afirmacao: [
+                    "Uma boa alimentação é a base de uma vida saudável.",
+                    "Frutas e vegetais devem ser a principal fonte de nutrientes na dieta."
+                ],
+                proxima: 1,
+            },
+            {
+                texto: "Evitar alimentos ultraprocessados.",
+                afirmacao: [
+                    "Evitar alimentos ultraprocessados é essencial para manter a saúde.",
+                    "Proteínas magras ajudam na construção e reparação dos tecidos do corpo."
+                ],
+                proxima: 2,
+            },
+        ]
+    },
+    {
+        enunciado: "Quais são benefícios das proteínas magras na alimentação?",
+        alternativas: [
+            {
+                texto: "Ajudam na construção e reparação dos tecidos.",
+                afirmacao: [
+                    "Grãos integrais fornecem fibras importantes para o sistema digestivo.",
+                    "O equilíbrio entre carboidratos, proteínas e gorduras é crucial para uma dieta saudável.",
+                    "Beber bastante água é fundamental para o bom funcionamento do organismo."
+                ],
+                proxima: 3,
+            },
+            {
+                texto: "Contribuem para a manutenção da massa muscular.",
+                afirmacao: [
+                    "Comer porções controladas pode ajudar a manter um peso saudável.",
+                    "Alimentos ricos em antioxidantes ajudam a proteger o corpo contra doenças.",
+                    "Evitar excessos de açúcar e sal pode prevenir problemas de saúde a longo prazo."
+                ],
+                proxima: 4,
+            },
+        ]
+    },
+    {
+        enunciado: "Por que é importante consumir grãos integrais?",
+        alternativas: [
+            {
+                texto: "Fornecem fibras que auxiliam na digestão.",
+                afirmacao: [
+                    "As gorduras saudáveis, como as encontradas no abacate e no azeite de oliva, são benéficas para o coração.",
+                    "A variedade na alimentação garante que você obtenha todos os nutrientes necessários.",
+                    "Alimentos frescos e naturais são sempre uma melhor escolha do que produtos processados."
+                ],
+                proxima: 3,
+            },
+            {
+                texto: "Oferecem vitaminas e minerais essenciais.",
+                afirmacao: [
+                    "A prática de uma alimentação equilibrada melhora a energia e o humor.",
+                    "Comer devagar e com atenção ajuda a reconhecer os sinais de saciedade.",
+                    "Incluir fontes de ômega-3, como peixes e sementes de chia, é ótimo para a saúde cerebral."
+                ],
+                proxima: 4,
+            },
+        ]
+    },
+    {
+        enunciado: "Quais são dois efeitos positivos da hidratação adequada?",
+        alternativas: [
+            {
+                texto: "Melhora a saúde da pele.",
+                afirmacao: [
+                    "A moderação é a chave para desfrutar de alimentos que você gosta sem comprometer a saúde.",
+                    "Lanches saudáveis, como nozes e iogurte, podem substituir opções menos nutritivas."
+                ],
+                proxima: 5,
+            },
+            {
+                texto: "Ajuda no bom funcionamento dos rins.",
+                afirmacao: [
+                    "Preparar as refeições em casa permite maior controle sobre os ingredientes e a qualidade dos alimentos.",
+                    "A ingestão adequada de cálcio é essencial para a saúde óssea."
+                ],
+                proxima: 6,
+            },
+        ]
+    },
+    {
+        enunciado: "Quais são vantagens de incluir gorduras saudáveis na dieta?",
+        alternativas: [
+            {
+                texto: "Beneficiam a saúde do coração.",
+                afirmacao: [
+                    "Incorporar legumes e verduras em todas as refeições é uma boa prática alimentar.",
+                    "Evitar bebidas açucaradas reduz o risco de diabetes e outros problemas de saúde."
+                ],
+                proxima: 5,
+            },
+            {
+                texto: "Auxiliam na função cerebral.",
+                afirmacao: [
+                    "O consumo de alimentos fermentados pode melhorar a saúde intestinal.",
+                    "A hidratação adequada contribui para a saúde da pele e o bom funcionamento dos rins."
+                ],
+                proxima: 6,
+            },
+        ]
+    },
+    {
+        enunciado: "Qual é o impacto de uma dieta rica em antioxidantes?",
+        alternativas: [
+            {
+                texto: "Protege o corpo contra doenças.",
+                afirmacao: [
+                    "A escolha de proteínas magras, como frango e peixe, pode ajudar a controlar o peso.",
+                    "Alimentos ricos em fibras, como frutas e grãos integrais, ajudam a manter a digestão saudável."
+                ],
+                proxima: 7,
+            },
+            {
+                texto: "Ajuda a combater o envelhecimento precoce das células.",
+                afirmacao: [
+                    "A moderação no consumo de cafeína e álcool também é importante para a saúde.",
+                    "A prática regular de exercícios complementa os benefícios de uma boa alimentação."
+                ],
+                proxima: 7,
+            },
+        ]
+    },
+    {
+        enunciado: "Quais são os benefícios de evitar excessos de açúcar e sal?",
+        alternativas: [
+            {
+                texto: "Reduz o risco de diabetes.",
+                afirmacao: [
+                    "O planejamento das refeições pode ajudar a evitar escolhas alimentares impulsivas e pouco saudáveis.",
+                    "Comer de forma equilibrada pode reduzir o risco de doenças cardiovasculares."
+                ],
+                proxima: 7,
+            },
+            {
+                texto: "Previne problemas de hipertensão.",
+                afirmacao: [
+                    "Os alimentos ricos em vitaminas e minerais fortalecem o sistema imunológico.",
+                    "Um café da manhã nutritivo fornece a energia necessária para o início do dia."
+                ],
+                proxima: 7,
+            },
+        ]
+    },
+    {
+        enunciado: "Por que comer devagar e com atenção é importante?",
+        alternativas: [
+            {
+                texto: "Ajuda a reconhecer os sinais de saciedade.",
+                afirmacao: [
+                    "Diversificar os tipos de alimentos e preparações enriquece a dieta e a saúde.",
+                    "A ingestão de gorduras boas, como as de castanhas e azeite, é importante para a função cerebral."
+                ],
+            },
+            {
+                texto: "Pode contribuir para uma melhor digestão dos alimentos.",
+                afirmacao: [
+                    "Manter um registro alimentar pode ajudar a identificar hábitos e promover mudanças positivas.",
+                    "A alimentação saudável é um investimento na qualidade de vida e bem-estar a longo prazo.",
+                    ""
+                ],
+            },
+        ]
+    }
+];
